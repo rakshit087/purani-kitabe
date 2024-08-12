2a89b42942082b2c9d8e3f64a957c72000f0f5d9
Rakshit <rakshit087@gmail.com>
2024-08-12 09:11:00 +0530

    feat: mock response in search

--- src/actions/search.ts
+++ src/actions/search.ts
@@ -1,5 +1,11 @@
 "use server";
 
 export const search = async (query: string) => {
-  console.log(query);
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  const mockResults = [
+    { title: `${query} - Result 1`, author: "Author 1" },
+    { title: `${query} - Result 2`, author: "Author 2" },
+    { title: `${query} - Result 3`, author: "Author 3" },
+  ];
+  return mockResults;
 };
